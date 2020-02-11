923c9f5871827c6fd4ea554e25aa5308c069f9ec
MUnehiroMachida <Life1231@outlook.jp>
2020-02-11 18:52:13 +0900

    animation前

--- labot_task/js/sample.js
+++ labot_task/js/sample.js
@@ -7,3 +7,18 @@ $(function () {
             $('.img-hover').fadeOut();
         });
 });
+
+
+
+
+// header
+    $(function () {
+        $('.navToggle').click(function () {
+            $(this).toggleClass('active');
+            if ($(this).hasClass('active')) {
+                $('.header-menu').addClass('active');
+            } else {
+                $('.header-menu').removeClass('active');
+            }
+        });
+    });
