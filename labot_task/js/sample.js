3538a633a5d4d8be02af377c35665fd1cd51b21e
MUnehiroMachida <Life1231@outlook.jp>
2020-02-13 23:56:40 +0900

    0213_fadein

--- labot_task/js/sample.js
+++ labot_task/js/sample.js
@@ -1,6 +1,6 @@
+// headerのはんばーがー
 $(function () {
-// header
-
+    // header
     $('.navToggle').click(function () {
         $(this).toggleClass('active');
         if ($(this).hasClass('active')) {
@@ -9,7 +9,6 @@ $(function () {
             $('.header-menu').removeClass('active');
         }
     });
-
     $('.news-img-item').hover(
         function () {
             $(this).find('.hover-slide').slideDown();
@@ -17,4 +16,49 @@ $(function () {
         function () {
             $(this).find('.hover-slide').slideUp();
         });
+});
+
+
+// index.htmlのhover
+$(function () {
+    $('#login-show').click(function () {
+        $('#login-modal').fadeIn();
+    });
+    $('.signup-show').click(function () {
+        $('#signup-modal').fadeIn();
+    });
+    $('.close-modal').click(function () {
+        $('#login-modal').fadeOut();
+        $('#signup-modal').fadeOut();
+    });
+    $('.lesson-hover').hover(
+        function () {
+            $(this).find('.text-contents').addClass('text-active');
+        },
+        function () {
+            $(this).find('.text-contents').removeClass('text-active');
+        }
+    );
+
+
+// inquiry.htmlのアコーディオン
+    $('.faq-list-item').click(function () {
+        var $answer = $(this).find('.answer');
+        if ($answer.hasClass('open')) {
+            $answer.removeClass('open');
+            $answer.fadeIn();
+            $(this).find('span').text('+');
+        } else {
+            $answer.addClass('open');
+            $answer.fadeOut();
+            $(this).find('span').text('-');
+        }
+    });
+});
+
+// フェードインクリック、かいてん
+$(function(){
+    $('.faq-list-item-box').click(function(){
+        $(this).toggleClass("faq-list-item-box img");
+    });
 });
