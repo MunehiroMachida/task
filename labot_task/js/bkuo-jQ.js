e14b0df92ffa6a2ce84716900014281f57e94bf1
MUnehiroMachida <Life1231@outlook.jp>
2020-02-24 20:04:34 +0900

    0222\バックアップ

---
+++ labot_task/js/bkuo-jQ.js
@@ -0,0 +1,39 @@
+// headerのはんばーがー
+$(function () {
+    // header
+    $('.navToggle').click(function () {
+        $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            $('.header-menu').addClass('active');
+        } else {
+            $('.header-menu').removeClass('active');
+        }
+    });
+    // hover
+    $('.news-img-item').hover(
+        function () {
+            $(this).find('.hover-slide').slideDown();
+        },
+        function () {
+            $(this).find('.hover-slide').slideUp();
+        });
+});
+
+// faq-listフェードイン
+$(function () {
+    $('.faq-list-item-box').click(function () {
+            $(this).toggleClass('open');
+            $(this).next('.answer').slideToggle();
+        });
+});
+// faq-list　img 回転
+$(function () {
+    $('.faq-list-item-box').click(function () {
+        $(this).children('p').toggleClass('action');
+        if ($(this).hasClass('action')) {
+            $(this).find('.faq-list-item-box p').addClass('action');
+        } else {
+            $(this).find('.faq-list-item-box p').removeClass('action');
+        }
+    });
+});
--- labot_task/js/sample.js
+++ labot_task/js/sample.js
@@ -1,43 +1,28 @@
-// headerのはんばーがー
-$(function () {
-    // header
-    $('.navToggle').click(function () {
-        $(this).toggleClass('active');
-        if ($(this).hasClass('active')) {
-            $('.header-menu').addClass('active');
-        } else {
-            $('.header-menu').removeClass('active');
-        }
-    });
-    // hover
-    $('.news-img-item').hover(
-        function () {
-            $(this).find('.hover-slide').slideDown();
-        },
-        function () {
-            $(this).find('.hover-slide').slideUp();
-        });
+const navToggle = document.getElementById('navToggle');
+const header_sp = document.getElementById('header-menu-sp');
+
+navToggle.addEventListener('click', () => {
+    navToggle.classList.toggle('active'); //ハンバーガーのアイコンの動き
+
+    //以下menuが出る動き
+    if(navToggle.classList.contains("active")){
+        header_sp.classList.add('active');
+    }else{
+        header_sp.classList.remove('active');
+    }
 });
-// faq-listフェードイン
-$(function () {
-    $('.faq-list-item-box').click(function () {
-            $(this).toggleClass('open');
-            $(this).next('.answer').slideToggle();
-        });
-});
-// faq-list　img 回転
-// $(function () {
-//     $('.faq-list-item-box').click(function () {
-//         $(this).children('p').toggleClass('action');
-//         if ($(this).hasClass('action')) {
-//             $(this).find('.faq-list-item-box p').addClass('action');
-//         } else {
-//             $(this).find('.faq-list-item-box p').removeClass('action');
-//         }
+
+
+// $('.news-img-item').hover(
+//     function () {
+//         $(this).find('.hover-slide').slideDown();
+//     },
+//     function () {
+//         $(this).find('.hover-slide').slideUp();
 //     });
 // });
 
-const img = document.getElementsByClassName('faq-list-item-box')[0];
-img.addEventListener('click', function() {
-    img.classList.add('action');
-});
+const news_img = document.getElementsByClassName('news-img-item')
+news_img.addEventListener('mouseover'), () => {
+    news_img.getElementsByClassName("hover-slide").classList.toggle('slideUp');
+}
